4cf4da0a9a289cd6df0da064c4fc896d3625b2f2
apeeapee <denbagus090774@gmail.com>
2025-10-13 16:13:27 +0700

    benerin merge lagi

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,9 @@ use App\Http\Controllers\Buyer\DashboardController as BuyerDashboardController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\OrderController as AdminOrderController;
 use App\Http\Controllers\Admin\CustomerController;
-<<<<<<< HEAD
-use App\Http\Controllers\CartController;
-=======
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\Buyer\OrderController as BuyerOrderController;
->>>>>>> main
 
 /*
 |--------------------------------------------------------------------------
@@ -25,41 +21,7 @@ use App\Http\Controllers\Buyer\OrderController as BuyerOrderController;
 // Rute Home
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
-// Rute untuk Admin
-
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-    Route::resource('products', ProductController::class)->names('admin.products');
-    // DITAMBAHKAN: Rute untuk menampilkan form tambah produk
-    Route::get('/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-
-    //Rute untuk menampilkan & update pesanan
-    Route::get('/orders', [OrderController::class, 'index'])->name('admin.orders.index');
-    Route::patch('/orders/{order}', [OrderController::class, 'update'])->name('admin.orders.update');
-
-    //Rute untuk menampilkan pelanggan
-    Route::get('/customers', [CustomerController::class, 'index'])->name('admin.customers.index');
-});
-
-// Rute untuk Pembeli (user)
-Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/buyer/dashboard', [BuyerDashboardController::class, 'index'])->name('buyer.dashboard');
-    // Tambahkan rute pembeli lainnya di sini...
-
-    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-    Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-    Route::patch('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
-    Route::delete('/cart/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
-
-    // Tambahkan route khusus untuk add to cart
-    Route::post('/cart/add', [CartController::class, 'store'])->name('cart.add');
-});
-
-//Rute untuk halaman "Tentang Kami"
-=======
 // Rute Tentang Kami
->>>>>>> main
 Route::get('/tentang-kami', function () {
     return view('about');
 })->name('about');
